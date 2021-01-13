05071cdb9f13d82975ac04f4d7e0f196a2d4c0f4
Andrea B <andrea.ballatore@gmail.com>
2021-01-13 18:33:48 +0000

    added webbot

--- conda_env/conda_export-mac.sh
+++ conda_env/conda_export-mac.sh
@@ -4,5 +4,9 @@ conda env list;
 # export environment to yml
 
 conda env export -n mip_v1 --from-history | grep -v "prefix" > mip_v1.yml;
+# works for regular pip packages
+echo "  - pip" >> mip_v1.yml;
+echo "  - pip:" >> mip_v1.yml;
+echo "    - webbot" >> mip_v1.yml;
 
-conda env export -n mip_v1 | grep -v "prefix" > mip_v1_full.yml;
+conda env export -n mip_v1 | grep -v "prefix" > mip_v1_full_mac.yml;
--- conda_env/conda_setup-mac.sh
+++ conda_env/conda_setup-mac.sh
@@ -13,7 +13,7 @@ conda create -n mip_v1 --channel anaconda python=3.8;
 
 # install packages 
 conda install --channel anaconda -n mip_v1 pandas scrapy numpy \
-	psycopg2 spacy nltk matplotlib scikit-learn scipy;
+	psycopg2 spacy nltk matplotlib scikit-learn scipy sqlite;
 
 # activate package
 echo "IMPORTANT - RUN: conda activate mip_v1";
