7142f1455639bf55224f93cfe832088110896dd6
Andrea B <andrea.ballatore@gmail.com>
2021-01-26 11:42:20 +0000

    added scraper

--- mip/scrapers/scraper_websites.py
+++ mip/scrapers/scraper_websites.py
@@ -17,4 +17,17 @@ from db.db import *
 def scrape_websites(museums_df):
     """ """
     print("scrape_websites", len(museums_df))
+
+    sample_df = pd.read_csv("data/museums/mip_data_sample_2020_01.tsv", sep='\t')
+    print("sample_df", len(sample_df))
+
+    for i in sample_df.index:
+        row = sample_df.loc[i]
+        scrape_website(row.mm_id,row.mm_name,row.website)
+
     
+def scrape_website(muse_id, muse_name, url):
+    assert muse_id
+    assert muse_name
+    assert url
+    print("scrape_website",url)
