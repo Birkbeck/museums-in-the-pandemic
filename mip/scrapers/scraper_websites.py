dec55c7e2c3b5112b80d6425607431a20ed2b4d8
Andrea B <andrea.ballatore@gmail.com>
2021-01-27 13:09:37 +0000

    tests

--- mip/scrapers/scraper_websites.py
+++ mip/scrapers/scraper_websites.py
@@ -25,7 +25,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 page_counter = 0
-user_agent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36"
+user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36"
 
 def scrape_websites(museums_df):
     """ Main """
--- mip/tests.py
+++ mip/tests.py
@@ -4,6 +4,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 import unittest
+import urllib
 
 """ 
 Unit Tests for all classes
@@ -19,3 +20,13 @@ class TestWebsiteScraper(unittest.TestCase):
     def test_2(self):
         assert 1 + 1 == 2
 
+    def test_problematic_sites(self):
+
+        user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36"
+        #user_agent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36"
+
+        for url in ['https://museumofcarpet.org','https://waterandsteam.org.uk/']:
+            req = urllib.request.Request(url, headers={'User-Agent': user_agent})
+            response = urllib.request.urlopen(req) #, timeout=5
+            new_url = response.geturl()
+            print(new_url)
