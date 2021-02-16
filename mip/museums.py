83b7ac9c98848c271a0dc2befb2292ed04056e5d
Andrea B <andrea.ballatore@gmail.com>
2021-02-16 17:40:19 +0000

    Update museums.py

--- mip/museums.py
+++ mip/museums.py
@@ -26,14 +26,29 @@ def load_input_museums():
 
 def get_fb_tw_links():
     """ Extract FB and TW links from Google results """
-    df = pd.read_csv('data/google_results/museum_searches-2021-02-05.tsv', sep='\t')
+    df = pd.read_csv('data/google_results/museum_searches_all-2021-02-16.tsv', sep='\t')
+    df = df[df.search_type == 'website']
     print(df.columns)
-    grouped = df.groupby('muse_id')
-    for muse_id, muse_df in grouped:
-        tw_df = muse_df[muse_df['domain'].str.match('twitter')]
-        fb_df = muse_df[muse_df['domain'].str.match('facebook')]
-        print(muse_id, len(muse_df), len(tw_df), len(fb_df))
-        # TODO
+    stats_df = pd.DataFrame()
+    res_df = pd.DataFrame()
+    for muse_id, muse_df in df.groupby('muse_id'):
+        tw_df = muse_df[muse_df['domain'].str.contains('twitter')]
+        fb_df = muse_df[muse_df['domain'].str.contains('facebook')]
+        muse_name = muse_df['Museum_Name'].tolist()[0]
+        assert muse_name
+        tw_urls = tw_df['url'].tolist()
+        fb_urls = fb_df['url'].tolist()
+        row = pd.DataFrame({'muse_id':muse_id, 'links_n':len(muse_df), 'twitter_link_n':len(tw_df), 'facebook_link_n':len(fb_df)}, index=[muse_id])
+        stats_df = stats_df.append(row)
+        # save fb/tw links
+        row = pd.DataFrame({'muse_id':muse_id, 'muse_name': muse_name, 'top_facebook': None, 'top_twitter': None}, index=[muse_id])
+        if len(tw_urls)>0: row['top_twitter'] = tw_urls[0]
+        if len(fb_urls)>0: row['top_facebook'] = fb_urls[0]
+        res_df = res_df.append(row)
+    print(res_df.describe())
+    print(res_df.sum())
+    print(res_df.isnull().sum(axis = 0))
+    res_df.to_csv('tmp/google_tw_fb_links_df.tsv', index=False, sep='\t')
 
 
 def load_extracted_museums():
