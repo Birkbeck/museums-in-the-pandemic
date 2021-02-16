4177856ffb9a34dfce93fe9f529084fea99c147d
Andrea B <andrea.ballatore@gmail.com>
2021-02-16 09:53:50 +0000

    Update museums.py

--- mip/db/museums.py
+++ mip/db/museums.py
@@ -16,3 +16,15 @@ def load_input_museums():
         raise ValueError("Duplicate museum names exist.")
     print("loaded museums:",len(df))
     return df
+
+
+
+def get_fb_tw_links():
+    """ Extract FB and TW links from Google results """
+    df = pd.read_csv('data/google_results/museum_searches-2021-02-05.tsv', sep='\t')
+    print(df.columns)
+    grouped = df.groupby('muse_id')
+    for muse_id, muse_df in grouped:
+        tw_df = muse_df[muse_df['domain'].str.match('twitter')]
+        fb_df = muse_df[muse_df['domain'].str.match('facebook')]
+        print(muse_id, len(muse_df), len(tw_df), len(fb_df))
