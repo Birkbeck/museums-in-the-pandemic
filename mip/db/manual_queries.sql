d10a2a71e0b673abfd1ae9d2affdeef0de01d1b8
Andrea B <andrea.ballatore@gmail.com>
2021-03-04 15:34:15 +0000

    Update manual_queries.sql

--- mip/db/manual_queries.sql
+++ mip/db/manual_queries.sql
@@ -20,15 +20,18 @@ SELECT table_name FROM information_schema.tables WHERE table_schema='websites';
 -- stats from dump tables
 select * from websites.web_pages_dump_20210303 limit 100;
 select page_id, url, referer_url, depth from websites.web_pages_dump_20210303 limit 100;
-select count(page_id) from websites.web_pages_dump_20210303;
-select muse_id, count(page_id) as page_n from websites.web_pages_dump_20210303 group by muse_id;
+select count(page_id) from websites.web_pages_dump_20210304;
+select muse_id, count(page_id) as page_n from websites.web_pages_dump_20210304 group by muse_id;
 
-select muse_id, url_domain, count(page_id) as page_n from websites.web_pages_dump_20210303 group by url_domain, muse_id;
+select muse_id, url_domain, count(page_id) as page_n from websites.web_pages_dump_20210304 group by url_domain, muse_id;
 
-select * from websites.web_pages_dump_20210303 wpd where is_start_url;
+-- how many museums
+select count(distinct muse_id) as muse_n from websites.web_pages_dump_20210304;
 
-select count(distinct muse_id) as muse_n from websites.web_pages_dump_20210303;
+select * from websites.web_pages_dump_20210304 wpd where is_start_url;
 
+
+https://marblebar.org.au/company/st-peters-heritage-centre-hall-1460398/
 select * from websites.web_pages_dump_20210303 where muse_id = 'mm.domus.SW005';
 -
 ------------------------------------------------
