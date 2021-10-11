823d3e5bff36a01801081155aaecc0a21713ddb0
Andrea B <andrea.ballatore@gmail.com>
2021-10-11 21:15:53 +0100

    Update scraper_twitter.py

--- mip/scrapers/scraper_twitter.py
+++ mip/scrapers/scraper_twitter.py
@@ -140,7 +140,7 @@ def scrape_twitter_account(muse_id, user_name, min_date, db_con):
         'tweet.fields': 'attachments, author_id, context_annotations, conversation_id, created_at, entities, geo, id, in_reply_to_user_id, lang, public_metrics, possibly_sensitive, referenced_tweets, reply_settings, source, text, withheld'.replace(' ',''),
         'expansions': 'attachments.poll_ids, attachments.media_keys, author_id, entities.mentions.username, geo.place_id, in_reply_to_user_id, referenced_tweets.id, referenced_tweets.id.author_id'.replace(' ',''),
         'start_time': start_time_iso,
-        'max_results': 100
+        'max_results': 200
         #'user.fields': 'created_at, description, entities, id, location, name, pinned_tweet_id, profile_image_url, protected, public_metrics, url, username, verified, withheld'.replace(' ',''),
         #'place.fields': 'contained_within, country, country_code, full_name, geo, id, name, place_type'.replace(' ',''),
         }
