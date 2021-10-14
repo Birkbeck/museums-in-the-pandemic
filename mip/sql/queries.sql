86614965f01152a748611acd9c879adf8dc9c8ab
Andrea B <andrea.ballatore@gmail.com>
2021-10-14 14:11:10 +0100

    Update queries.sql

--- mip/sql/queries.sql
+++ mip/sql/queries.sql
@@ -1,3 +1,9 @@
 
+-- Manual SQL queries for MIP
 
 select muse_id,count(*) from twitter.tweets_dump group by muse_id ;
+
+select count(distinct muse_id) from twitter.tweets_dump;
+
+select count(*) from twitter.twitter_accounts_not_found;
+
