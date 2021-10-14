a78c49bd0903d65e39aa5290c9fd27f990ccd6e4
Andrea B <andrea.ballatore@gmail.com>
2021-10-14 18:43:09 +0100

    bug

--- mip/sql/queries.sql
+++ mip/sql/queries.sql
@@ -1,16 +1,15 @@
 
 -- Manual SQL queries for MIP
 
+-- twitter 
 select muse_id,count(*) from twitter.tweets_dump group by muse_id ;
-
 select count(distinct muse_id) from twitter.tweets_dump;
-
 select count(*) from twitter.twitter_accounts_not_found;
 select count(*) from twitter.tweets_dump td ;
-
 select * from twitter.twitter_accounts_not_found tanf;
 
+-- facebook
 select count(*) from facebook.facebook_posts_dump;
-
+select museum_id, count(*) from facebook.facebook_posts_dump group by museum_id;
 select count(distinct museum_id) from facebook.facebook_posts_dump;
 select count(distinct page_name) from facebook.facebook_pages_not_found;
