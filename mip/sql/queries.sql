5c75910b99cdcd9a70af5646271c9240c5961efa
Andrea B <andrea.ballatore@gmail.com>
2021-10-14 18:15:28 +0100

    bug fix

--- mip/sql/queries.sql
+++ mip/sql/queries.sql
@@ -9,3 +9,8 @@ select count(*) from twitter.twitter_accounts_not_found;
 select count(*) from twitter.tweets_dump td ;
 
 select * from twitter.twitter_accounts_not_found tanf;
+
+select count(*) from facebook.facebook_posts_dump;
+
+select count(distinct museum_id) from facebook.facebook_posts_dump;
+select count(distinct page_name) from facebook.facebook_pages_not_found;
