0176d42f86c064026f818f901163612d83c7354b
Andrea B <andrea.ballatore@gmail.com>
2021-11-23 12:42:38 +0000

    mip search app

--- mip/mip_search_app/google_collab_backup/search_utils.py
+++ mip/mip_search_app/google_collab_backup/search_utils.py
@@ -31,7 +31,8 @@ stop_words.extend([".",",",";","'",'//',"'s"])
 stop_words = set(stop_words)
 
 def open_local_db():
-  conn = sqlite3.connect('mip_corpus_search-test2.db')
+  #conn = sqlite3.connect('mip_corpus_search-test2.db')
+  conn = sqlite3.connect('mip_corpus_search.db')
   return conn
 
 def filter_search_string_for_sql(text):
@@ -45,34 +46,59 @@ def filter_search_string_for_regex(text, case_sensitive):
   return text
 
 def run_search(text, case_sensitive, search_facebook, search_twitter,
-  search_websites):
+  search_websites, search_website_sentences):
   assert len(text) > 3, 'search string too short!'
   assert search_facebook or search_twitter or search_websites, 'select at least one platform'
+  if search_websites and search_website_sentences:
+    raise Exception('select either search_websites or search_website_sentences, not both')
   where = ''
   platforms = []
-  if search_websites: platforms.append('website')
+  # search websites
+  if search_websites:
+    sql = "select * from websites_text where page_text like '%{}%';".format(filter_search_string_for_sql(text))
+    web_df = pd.read_sql(sql, db_conn)
+    n_u_museums = web_df.museum_id.nunique()
+    web_df['platform'] = 'website'
+    web_df['session_time'] = web_df['session_id'].apply(sessionid_to_time)
+    print('Websites: {} matches found. Unique museums: {} - N sessions: {}'.format(len(web_df), n_u_museums, web_df.session_id.nunique()))
+  
+  if search_website_sentences:
+    sql = "select * from websites_sentences_text where sentence_text like '%{}%';".format(filter_search_string_for_sql(text))
+    web_df = pd.read_sql(sql, db_conn)
+    n_u_museums = web_df.museum_id.nunique()
+    web_df['platform'] = 'website_sentences'
+    web_df['session_time'] = web_df['session_id'].apply(sessionid_to_time)
+    print('Website sentences: {} matches found. Unique museums: {} - N sessions: {}'.format(len(web_df), n_u_museums, web_df.session_id.nunique()))
+  
+  # search social media
   if search_facebook or search_twitter:
-    # search social media
     if search_facebook: platforms.append('facebook')
     if search_twitter: platforms.append('twitter')
     where = ','.join(["'"+x+"'" for x in platforms])
     sql = "select * from social_media_msg where platform in ({}) and msg_text like '%{}%';".format(where, 
       filter_search_string_for_sql(text))
+    
+    soc_df = pd.read_sql(sql, db_conn)
+    n_u_museums = soc_df.museum_id.nunique()
+    print('Social media:',len(soc_df),'matches found. Unique museums:',n_u_museums)
 
-    #print(sql)
-    df = pd.read_sql(sql, db_conn)
-    print('N results:',len(df))
-  
+  df = merge_results(web_df, soc_df)
   return df
 
 def get_before_after_strings(s, regex, context_size_words):
-  #print('get_before_after_strings:', s)
-  mm = re.finditer(regex, s)
+  #print('get_before_after_strings:', regex)
+  assert len(regex) > 1
+  assert len(s) > 0
   sep = ' '
-  for m in mm:
-      beg, end = m.span()
+  assert context_size_words > 0 and context_size_words < 1000
+  for m in re.finditer(regex, s):
+      beg, end = m.start(), m.end()
+      #print(m, beg, end)
+      assert beg >= 0
+      assert end >= 0
       bef = s[0:beg]
       aft = s[end+1:-1]
+      
       bef_words = bef.split(sep)
       #print(bef_words)
       if len(bef_words) > context_size_words:
@@ -80,10 +106,19 @@ def get_before_after_strings(s, regex, context_size_words):
       aft_words = aft.split(sep)
       if len(aft_words) > context_size_words:
         aft_words = aft_words[0:context_size_words]
+      
       assert len(bef_words) <= context_size_words
       assert len(aft_words) <= context_size_words
       #print(bef_words, aft_words)
-      return bef_words, m.group(), aft_words
+      assert len(bef_words) >= 0
+      assert len(aft_words) >= 0
+      match_str = m.group()#.strip()
+      #print('match_str',match_str)
+      assert match_str
+      #print(bef_words, match_str, aft_words)
+      return bef_words, match_str, aft_words
+  # nothing found
+  return None, None, None
 
 def get_now_string():
   now = datetime.now()
@@ -91,7 +126,30 @@ def get_now_string():
   current_time = now.strftime("%Y%m%d-%H%M%S")
   return current_time
 
-def generate_html_matches(df, search_string, case_sensitive, context_size_words, max_results):
+def sessionid_to_time(session_id):
+  dd = datetime.strptime(session_id, '%Y%m%d')
+  return dd
+
+def merge_results(web_df, soc_df):
+  #print('merge_results')
+  #print(soc_df.columns)
+  #print(web_df.columns)
+  # Index(['museum_id', 'account', 'msg_text', 'msg_time', 'platform'], dtype='object')
+  # Index(['museum_id', 'museum_name', 'session_id', 'page_id', 'url', 'sentence_id', 'sentence_text']
+  if len(web_df)>0:
+    #web_df = web_df.rename(columns={'msg_text':'sentence_text','msg_time':'session_id'})
+    if 'sentence_text' in web_df.columns: 
+      web_df['msg_text'] = web_df['sentence_text']
+    if 'page_text' in web_df.columns:
+      web_df['msg_text'] = web_df['page_text']
+    if 'session_id' in web_df.columns:
+      web_df['msg_time'] = web_df['session_time']
+  
+  df = pd.concat([web_df,soc_df], axis=0, ignore_index=True)
+  #print(df)
+  return df
+
+def generate_html_matches(res_df, search_string, case_sensitive, context_size_words, max_results):
   """
   <tr>
     <td>Alfreds Futterkiste</td>
@@ -112,40 +170,49 @@ def generate_html_matches(df, search_string, case_sensitive, context_size_words,
     border-collapse: collapse;
     font-family: sans-serif;
     vertical-align: top;
-    }
+  }
   .before_col { text-align: right; }
+  .match_col { font-weight: bold; color: blue; }
   strong { background-color: blue; color: white; }
   </style>"""
-  j = 0
+  # ==== generate results ====
   results_page_d = []
   search_regex = filter_search_string_for_regex(search_string, case_sensitive)
-  print("search_regex:",search_regex)
-  
-  for i, r in df.iterrows():
-    j += 1
-    bef_words, match_text, aft_words = get_before_after_strings(r['msg_text'], search_regex, context_size_words)
-    results_page_d.append({'res':j, 'museum_id':r['museum_id'], 
-      'before':' '.join(bef_words), 'match': match_text,
-      'after':' '.join(aft_words), 'platform':r['platform'] })
+  print("search_regex: '{}'".format(search_regex))
+  #df = merge_results(soc_df, web_df)
+  for nn, subdf in res_df.groupby('platform'):
+    j = 0
+    for i, r in subdf.iterrows():
+      j += 1
+      msg_txt = r['msg_text'].strip()
+      assert len(msg_txt) > 0
+      bef_words, match_text, aft_words = get_before_after_strings(msg_txt, search_regex, context_size_words)
+      if match_text is None: continue 
+      results_page_d.append({'res':j, 'museum_id':r['museum_id'], 
+        'before':' '.join(bef_words), 'match': match_text, 'msg_time':r.msg_time,
+        'after':' '.join(aft_words), 'platform':r['platform'] })
   results_page_df = pd.DataFrame(results_page_d)
 
-  header = "<tr>" + ''.join(["<th>{}</th>".format(x) for x in results_page_df.columns]) + "</tr>"
-  table_rows_h = ''
-  j = 0
-  for idx, row in results_page_df.iterrows():
-    j += 1
-    if j > max_results: break
-    #for m in mm:
-    #repl_str = '<strong>{}</strong>'.format(match_text)
-    #html_m = html_m.replace(match_text, repl_str)
-    row_h = ''
-    for c in results_page_df.columns:
-      css_class = ''
-      if 'before' in c:
-        css_class='before_col'
-      row_h += '<td class="{}">{}</td>'.format(css_class, row[c])
-    table_rows_h += "<tr>{}</tr>".format(row_h)
-  h = css + """<table>""" + header + table_rows_h + "</table>"
+  # ==== generate HTML from results ====
+  columns_table_html = ['res','museum_id','before','match','after','msg_time']
+  header = "<tr>" + ''.join(["<th>{}</th>".format(x) for x in columns_table_html]) + "</tr>"
+  h = css
+  for plat_name, subdf in results_page_df.groupby('platform'):
+    j = 0
+    table_rows_h = ''
+    for idx, row in subdf.iterrows():
+      j += 1
+      if j > max_results: break
+      row_h = ''
+      for c in columns_table_html:
+        css_class = ''
+        if 'before' in c:
+          css_class='before_col'
+        if 'match' in c:
+          css_class='match_col'
+        row_h += '<td class="{}">{}</td>'.format(css_class, row[c])
+      table_rows_h += "<tr>{}</tr>".format(row_h)
+    h += "<h3>{}</h3><table>{}{}</table>".format(plat_name, header, table_rows_h)
   return h, results_page_df
 
 def filter_tokens(tokens):
